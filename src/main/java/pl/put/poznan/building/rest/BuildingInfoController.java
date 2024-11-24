4c5a6b5e1f80092c5648445ec931078ce5723c82
helag7 <helagarside@gmail.com>
2024-11-24 22:00:22 +0100

    Upload project template files

---
+++ src/main/java/pl/put/poznan/building/rest/BuildingInfoController.java
@@ -0,0 +1,46 @@
+package pl.put.poznan.building.rest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+import pl.put.poznan.building.logic.BuildingInfo;
+
+import java.util.Arrays;
+
+
+@RestController
+@RequestMapping("/{text}")
+public class BuildingInfoController {
+
+    private static final Logger logger = LoggerFactory.getLogger(BuildingInfoController.class);
+
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    public String get(@PathVariable String text,
+                              @RequestParam(value="transforms", defaultValue="upper,escape") String[] transforms) {
+
+        // log the parameters
+        logger.debug(text);
+        logger.debug(Arrays.toString(transforms));
+
+        // perform the transformation, you should run your logic here, below is just a silly example
+        BuildingInfo transformer = new BuildingInfo(transforms);
+        return transformer.transform(text);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
+    public String post(@PathVariable String text,
+                      @RequestBody String[] transforms) {
+
+        // log the parameters
+        logger.debug(text);
+        logger.debug(Arrays.toString(transforms));
+
+        // perform the transformation, you should run your logic here, below is just a silly example
+        BuildingInfo transformer = new BuildingInfo(transforms);
+        return transformer.transform(text);
+    }
+
+
+
+}
+
+
