854c0ef3161f7acc5e409e9b32be0d76c46d97a2
helag <helagarside@gmail.com>
2024-12-06 18:13:05 +0100

    GET method for the building added

--- src/main/java/pl/put/poznan/building/rest/BuildingController.java
+++ src/main/java/pl/put/poznan/building/rest/BuildingController.java
@@ -1,15 +1,12 @@
 package pl.put.poznan.building.rest;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import pl.put.poznan.building.models.Building;
 import pl.put.poznan.building.models.Floor;
 import pl.put.poznan.building.models.Room;
 
-
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,6 +32,20 @@ public class BuildingController {
         logger.info("Building added: {}", building);
 
         return building;
+    }
+
+    @GetMapping("/{id}")
+    public Building getBuilding(@PathVariable String id) {
+        logger.info("Trying to access building with id: {}", id);
+
+        for (Building building: buildingStorage){
+            if (building.getId().equals(id)){
+                logger.info("Successfully accessed building with id: {}", id);
+                return building;
+            }
+        }
+        //Exception if building was not found:
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Building not found");
     }
 
 
