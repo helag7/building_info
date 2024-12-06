a7a747a91afaf2537e8bac79c7f42a18266b7029
helag <helagarside@gmail.com>
2024-12-06 18:13:32 +0100

    scanBasePackages corrected

--- src/main/java/pl/put/poznan/building/app/BuildingInfoApplication.java
+++ src/main/java/pl/put/poznan/building/app/BuildingInfoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@SpringBootApplication(scanBasePackages = {"pl.put.poznan.transformer.rest"})
+@SpringBootApplication(scanBasePackages = {"pl.put.poznan.building.rest"})
 public class BuildingInfoApplication {
 
     public static void main(String[] args) {
